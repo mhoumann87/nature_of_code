3d04706a475a23b17f955a25ec11cc6960bc449a
Michael Houmann <mhoumann87@gmail.com>
2021-09-25 15:34:59 +0200

    Add noiseDetail()

--- 00_starting_out/05_noiseDetail/sketch.js
+++ 00_starting_out/05_noiseDetail/sketch.js
@@ -1,31 +1,24 @@
-// Value used to increment the off sets
 let inc = 0.01;
 
 function setup() {
   createCanvas(200, 200);
-
-  // for this to work on high density screens, use this function
-  pixelDensity(1);
 }
 
 function draw() {
   background(51);
+  noiseDetail(8, 0.2);
+
+  loadPixels();
 
   let xoff = 0;
-  loadPixels();
 
   for (let x = 0; x < width; x++) {
     let yoff = 0;
     for (let y = 0; y < height; y++) {
       let i = (x + y * width) * 4;
 
-      // use a random value for color
-      // gives static like an old tv between stations
-      // const r = random(255);
-
-      // We to get noise values and need to off set the between the loops
       const r = noise(xoff, yoff) * 255;
-      // color the pixel
+
       pixels[i + 0] = r; // red
       pixels[i + 1] = r; // green
       pixels[i + 2] = r; // blue
