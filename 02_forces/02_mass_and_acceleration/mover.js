043eceeb9497184223a9016d4c93b56f32c01ce8
Michael Houmann <mhoumann87@gmail.com>
2021-10-05 11:17:11 +0200

    Finish mass and acceleration section

--- 02_forces/02_mass_and_acceleration/mover.js
+++ 02_forces/02_mass_and_acceleration/mover.js
@@ -1,9 +1,14 @@
 class Mover {
-  constructor(x, y) {
+  constructor(x, y, m) {
     this.position = createVector(x, y);
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
-    this.r = 16;
+    this.mass = m;
+    // To see the difference between the movers, we can ude the area
+    // of the circle to display the difference in "weight". To calculate the
+    // area we can take the square root of the mass and use that as r. This will be very small, so we multiply it with an arbitrary value
+    this.r = sqrt(this.mass) * 10;
+    // Now the "bounce" will be different, but that is because they "land" at different times.
   }
 
   edges() {
@@ -22,7 +27,8 @@ class Mover {
   }
 
   applyForce(force) {
-    this.acceleration.add(force);
+    const f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
   }
 
   update() {
--- 02_forces/02_mass_and_acceleration/sketch.js
+++ 02_forces/02_mass_and_acceleration/sketch.js
@@ -3,8 +3,8 @@ let moverA, moverB;
 function setup() {
   createCanvas(400, 400);
 
-  moverA = new Mover(100, height * 0.5);
-  moverB = new Mover(300, height * 0.5);
+  moverA = new Mover(100, height * 0.5, 2);
+  moverB = new Mover(300, height * 0.5, 4);
 }
 
 function draw() {
@@ -12,8 +12,17 @@ function draw() {
 
   // Gravity pulling object straight down
   const gravity = createVector(0, 0.2);
-  moverA.applyForce(gravity);
-  moverB.applyForce(gravity);
+  // The gravitational acceleration is based on the weight of the object,
+  // so we have to add the "weight" of the object based on it's mass
+  const weightA = p5.Vector.mult(gravity, moverA.mass);
+  const weightB = p5.Vector.mult(gravity, moverB.mass);
+
+  moverA.applyForce(weightA);
+  moverB.applyForce(weightB);
+
+  // Gravity gives the two objects the same speed falling, but the wind
+  // interacts with the differently, the "heavier" one needs more force
+  // to affect it
 
   // Wind, when added are pushing object to right
   if (mouseIsPressed) {
