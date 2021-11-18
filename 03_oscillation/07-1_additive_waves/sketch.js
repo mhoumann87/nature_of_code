ea726ff10fd94b18a18c6cb40c1ca0d49fe45aa5
Michael Houmann <mhoumann87@gmail.com>
2021-11-18 13:18:07 +0100

    Add section 3.7, additive wave

---
+++ 03_oscillation/07-1_additive_waves/sketch.js
@@ -0,0 +1,26 @@
+let waves = [];
+
+function setup() {
+  createCanvas(600, 400);
+
+  for (let i = 0; i < 5; i++) {
+    waves[i] = new Wave(random(20, 80), random(100, 600), random(TWO_PI));
+  }
+}
+
+function draw() {
+  background(0);
+
+  for (let x = 0; x < innerWidth; x += 10) {
+    let y = 0;
+    for (let wave of waves) {
+      y += wave.evaluate(x);
+    }
+    noStroke();
+    ellipse(x, y + height / 2, 10);
+  }
+
+  for (let wave of waves) {
+    wave.update();
+  }
+}
